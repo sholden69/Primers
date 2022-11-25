e6735bea799e4eae9324b044fa1ebd87e844ae2d
Simon Holden <simon.holden@gmail.com>
2022-11-25 18:32:30 +0000

    setup

---
+++ blank_main.py
@@ -0,0 +1,12 @@
+# blank_main.py
+# A blank main file with the basics in to build on
+
+
+#import  x as blah
+
+
+def main_func(param1):
+    print("do something")
+
+if __name__ == "__main__":
+    main_func("hi")
